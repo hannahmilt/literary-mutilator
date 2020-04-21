b53d32dc2f4d2eb476f02d91f7e00b7aeff570f2
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-20 21:50:33 -0600

    set up for the challenge tried changing color of text (didn't work)

---
+++ epic/script.js
@@ -0,0 +1,19 @@
+//var str = new String("Cat ipsum dolor sit amet, purr like an angel purr but sleep all day whilst slave is at work, play all night whilst slave is sleeping. Show belly allways wanting food for man running from cops stops to pet cats, goes to jail. Lick face hiss at owner, pee a lot, and meow repeatedly scratch at fence purrrrrr eat muffins and poutine until owner comes back love blinks and purr purr purr purr yawn sleep on my human's head. 𝕄𝔼𝕆𝕎 kitty loves pigs or sweet beast, for i am the best but love to play with owner's hair tie yet run around the house at 4 in the morning, why use post when this sofa is here. Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life this human feeds me, i should be a god kitty loves pigs jump up to edge of bath, fall in then scramble in a mad panic to get out terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry decide to want nothing to do with my owner today. This human feeds me, i should be a god plays league of legends. Sleeps on my head. Eat the rubberband play riveting piece on synthesizer keyboard. Catty ipsum jump around on couch,");
+//document.write(str.fontcolor( "blue" ));
+//function init() {
+//	document.getElementById("p").style.color = 'blue';
+//}
+
+/*function highlight(text) {
+	var inputText = document.getElementById("inputText");
+	var innerHTML = inputText.innerHTML;
+	var index = innerHTML.indexOf(text);
+	if (index >= 0) {
+		innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+		inputText.innerHTML = innerHTML;
+	}
+}*/
+
+let para = document.getElementById('para');
+console.log(para.style.color);
+one.style.color = "magenta";
